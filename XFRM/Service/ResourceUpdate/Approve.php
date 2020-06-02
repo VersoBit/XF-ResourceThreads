863a183a89330d2fb14e6e0da1e3798fb3a9fae7
Tom Coward <tom@versobit.com>
2020-06-02 02:41:19 +0100

    Updated post finder conditions

--- XFRM/Service/ResourceUpdate/Approve.php
+++ XFRM/Service/ResourceUpdate/Approve.php
@@ -16,14 +16,15 @@ class Approve extends XFCP_Approve
     protected function approveDiscussionThreadPost($update)
     {
         // TODO: find more solid way of finding the update's post in discussion thread
+        $updateUrl = '%resources/'. strtolower($update->title) .'.'. $update->Resource->resource_id .'/update/'. $update->resource_update_id .'/%';
         $post = \XF::finder('XF:Post')->where([
             'thread_id' => $update->Resource->discussion_thread_id,
-            'post_date' => $update->post_date,
-            'user_id' => $update->Resource->user_id
+            'user_id' => $update->Resource->user_id,
+            ['message', 'LIKE', $updateUrl]
         ])->fetchOne();
 
         // Approve resource update's associated post if unapproved
-        if($post->message_state == 'moderated'){
+        if($post AND $post->message_state == 'moderated'){
             /** @var \XF\Service\Post\Approver $postApprover */
             $postApprover = \XF::service('XF:Post\Approver', $post);
             $postApprover->approve();
